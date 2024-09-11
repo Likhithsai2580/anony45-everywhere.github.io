519079e62ae7e6b5bfb8fc3fb4259a0e537c25ce
hackersareherewhereareyou <semalalikithsai@gmail.com>
2024-09-11 11:01:45 +0530

    revert

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    assetPrefix: '/anony45-everywhere.github.io/',
+    basePath: '/anony45-everywhere.github.io',
+    trailingSlash: true,
+  };
+  
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/404': { page: '/404' },
+      // Add other routes here
+    }
   },
-  // Add this line:
-  trailingSlash: true,
-  basePath: '/anony45-everywhere.github.io',
-};
-
-module.exports = nextConfig;
+}
