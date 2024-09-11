4c8cd4894283356893a995b5bd116d9d092c15cf
hackersareherewhereareyou <semalalikithsai@gmail.com>
2024-09-11 11:33:01 +0530

    last i think

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ const nextConfig = {
     assetPrefix: '/anony45-everywhere.github.io/',
     basePath: '/anony45-everywhere.github.io',
     trailingSlash: true,
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = { fs: false };
+      }
+      return config;
+    },
   };
   
   module.exports = nextConfig;
