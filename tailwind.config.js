30598ad82797a9fcb259967e39e5704f3e111a00
hackersareherewhereareyou <semalalikithsai@gmail.com>
2024-09-11 10:19:17 +0530

    test 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
   purge: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
