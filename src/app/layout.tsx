95686bb60388ef5ff781b60945e9d461809887f7
Likhithsai2580 <semalalikithsai@gmail.com>
2024-10-02 20:04:13 +0530

    Revert "last i think"
    This reverts commit 4c8cd4894283356893a995b5bd116d9d092c15cf.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from 'next-themes';
 
-const poppins = Poppins({ 
-  weight: ['400', '600', '700'], 
-  subsets: ["latin"],
-  display: 'swap',
-});
+const poppins = Poppins({ weight: ['400', '600', '700'], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Portfolio by Likhith Sai",
@@ -21,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+      </head>
       <body className={poppins.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
