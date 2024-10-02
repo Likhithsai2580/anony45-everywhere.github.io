18124873acfd4839388a8b906a8c15811c0cccac
Likhithsai2580 <semalalikithsai@gmail.com>
2024-10-02 20:04:17 +0530

    Revert "revert"
    This reverts commit 519079e62ae7e6b5bfb8fc3fb4259a0e537c25ce.

--- next.config.js
+++ next.config.js
@@ -1,23 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    assetPrefix: '/anony45-everywhere.github.io/',
-    basePath: '/anony45-everywhere.github.io',
-    trailingSlash: true,
-  };
-  
-module.exports = {
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/404': { page: '/404' },
-      // Add other routes here
-    }
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
-}
+  // Add this line:
+  trailingSlash: true,
+  basePath: '/anony45-everywhere.github.io',
+};
+
+module.exports = nextConfig;
